5da4c7bcc104c7834eb46205d9936be84fbd01dd
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2017-01-18 16:06:58 +0100

    Push poorly written test can down the road another ten years

---
+++ menu.php
@@ -0,0 +1,150 @@
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+            <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Buscar...">
+              <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="../img/kyros_v2.png" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Menú principal</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <!--li class="header">Menú principal</li-->
+
+<li class="treeview">
+          <a href="#">
+            <i class="fa fa-share"></i> <span>CPDs</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li>
+              <a href="#"><i class="fa fa-circle-o"></i>UVA
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="../CPD/trafico_uva.php"><i class="fa fa-circle-o"></i>Tráfico</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-circle-o"></i>Oficina
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="../CPD/trafico_oficina.php"><i class="fa fa-circle-o"></i>Tráfico</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-circle-o"></i>Rally
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li><a href="../CPD/trafico_sit.php"><i class="fa fa-circle-o"></i>Tráfico SIT</a></li>
+              </ul>
+              <ul class="treeview-menu">
+                <li><a href="../CPD/trafico_camion_http.php"><i class="fa fa-circle-o"></i>Tráfico Camión (http)</a></li>
+              </ul>
+              <ul class="treeview-menu">
+                <li><a href="../CPD/trafico_camion_https.php"><i class="fa fa-circle-o"></i>Tráfico Camión (https)</a></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Producción</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../produccion/map.php"><i class="fa fa-circle-o"></i>Arquitectura</a></li>
+            <li><a href="../produccion/estado.php"><i class="fa fa-circle-o"></i>Estado</a></li>
+            <li><a href="../produccion/estadisticas.php"><i class="fa fa-circle-o"></i>Estadisticas</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Correos</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../correos/map.php"><i class="fa fa-circle-o"></i>Arquitectura</a></li>
+            <li><a href="../correos/estado.php"><i class="fa fa-circle-o"></i>Estado</a></li>
+            <li><a href="../correos/estadisticas.php"><i class="fa fa-circle-o"></i>Estadisticas</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Hawkeye</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../hawkeye/map.php"><i class="fa fa-circle-o"></i>Arquitectura</a></li>
+            <li><a href="../hawkeye/estado.php"><i class="fa fa-circle-o"></i>Estado</a></li>
+            <li><a href="../hawkeye/estadisticas.php"><i class="fa fa-circle-o"></i>Estadisticas</a></li>
+          </ul>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>SUMO</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../sumo/map.php"><i class="fa fa-circle-o"></i>Arquitectura</a></li>
+            <li><a href="../sumo/estado.php"><i class="fa fa-circle-o"></i>Estado</a></li>
+            <li><a href="../sumo/estadisticas.php"><i class="fa fa-circle-o"></i>Estadisticas</a></li>
+          </ul>
+        </li>
+
+
+        <li>
+          <a href="../otros/calendario.php">
+            <i class="fa fa-calendar"></i> <span>Calendar</span>
+          </a>
+        </li>
+        
+       
+        
+        <li><a href="https://wiki.kyroslbs.com"><i class="fa fa-book"></i> <span>Documentación</span></a></li>
+        <li class="header">Herramientas</li>
+        <li><a href="https://jira.kyroslbs.com"><i class="fa fa-circle-o text-red"></i> <span>Jira</span></a></li>
+        <li><a href="https://cloud.kyroslbs.com"><i class="fa fa-circle-o text-yellow"></i> <span>Cloud</span></a></li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
